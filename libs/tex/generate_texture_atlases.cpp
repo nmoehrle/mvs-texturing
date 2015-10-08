nmoehrle/mvs-texturing

commit 40a0e42f3e2920b8a2cc2afc50d7476276b49cce

Create texture atlases independent of model type

libs/tex/generate_texture_atlases.cpp
@@ -1,0 +1,191 @@
+/*
+ * Copyright (C) 2015, Nils Moehrle
+ * TU Darmstadt - Graphics, Capture and Massively Parallel Computing
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD 3-Clause license. See the LICENSE.txt file for details.
+ */
+
+#include <set>
+#include <list>
+#include <iostream>
+#include <fstream>
+
+#include <util/timer.h>
+#include <mve/image_tools.h>
+
+
+#include "defines.h"
+#include "histogram.h"
+#include "texture_patch.h"
+#include "texture_atlas.h"
+
+#define MAX_TEXTURE_SIZE (8 * 1024)
+#define PREF_TEXTURE_SIZE (4 * 1024)
+#define MIN_TEXTURE_SIZE (256)
+
+TEX_NAMESPACE_BEGIN
+
+/**
+  * Heuristic to calculate an appropriate texture atlas size.
+  * @warning asserts that no texture patch exceeds the dimensions
+  * of the maximal possible texture atlas size.
+  */
+unsigned int
+calculate_texture_size(std::list<TexturePatch::ConstPtr> const & texture_patches) {
+    unsigned int size = MAX_TEXTURE_SIZE;
+
+    while (true) {
+        unsigned int total_area = 0;
+        unsigned int max_width = 0;
+        unsigned int max_height = 0;
+        unsigned int padding = size >> 7;
+
+        for (TexturePatch::ConstPtr texture_patch : texture_patches) {
+            unsigned int width = texture_patch->get_width() + 2 * padding;
+            unsigned int height = texture_patch->get_height() + 2 * padding;
+
+            max_width = std::max(max_width, width);
+            max_height = std::max(max_height, height);
+
+            unsigned int area = width * height;
+            unsigned int waste = area - texture_patch->get_size();
+
+            if (static_cast<double>(waste) / texture_patch->get_size() > 1.0) {
+                break;
+            }
+
+            total_area += area;
+        }
+
+        assert(max_width < MAX_TEXTURE_SIZE);
+        assert(max_height < MAX_TEXTURE_SIZE);
+        if (size > PREF_TEXTURE_SIZE &&
+            max_width < PREF_TEXTURE_SIZE &&
+            max_height < PREF_TEXTURE_SIZE &&
+            total_area / (PREF_TEXTURE_SIZE * PREF_TEXTURE_SIZE) < 8) {
+            size = PREF_TEXTURE_SIZE;
+            continue;
+        }
+
+        if (size <= MIN_TEXTURE_SIZE) {
+            return MIN_TEXTURE_SIZE;
+        }
+
+        if (max_height < size / 2 && max_width < size / 2 &&
+            static_cast<double>(total_area) / (size * size) < 0.2) {
+            size = size / 2;
+            continue;
+        }
+
+        return size;
+    }
+}
+
+std::pair<float, float>
+calculate_mapping_function(std::list<TexturePatch::ConstPtr> const & texture_patches) {
+    float min = std::numeric_limits<float>::max();
+    float max = std::numeric_limits<float>::lowest();
+    for (TexturePatch::ConstPtr texture_patch : texture_patches) {
+        mve::FloatImage::ConstPtr image = texture_patch->get_image();
+        mve::ByteImage::ConstPtr validity_mask = texture_patch->get_validity_mask();
+        for (int i = 0; i < image->get_value_amount(); ++i) {
+            if (validity_mask->at(i / 3) == 0) continue;
+
+            min = std::min(min, image->at(i));
+            max = std::max(max, image->at(i));
+        }
+    }
+    Histogram hist(min, max, 10000);
+    for (TexturePatch::ConstPtr texture_patch : texture_patches) {
+        mve::FloatImage::ConstPtr image = texture_patch->get_image();
+        mve::ByteImage::ConstPtr validity_mask = texture_patch->get_validity_mask();
+        for (int i = 0; i < image->get_value_amount(); ++i) {
+            if (validity_mask->at(i / 3) == 0) continue;
+
+            hist.add_value(image->at(i));
+        }
+    }
+    min = hist.get_approx_percentile(0.005f);
+    max = hist.get_approx_percentile(0.995f);
+
+    return std::pair<float, float>(min, max);
+}
+
+void
+generate_texture_atlases(std::vector<TexturePatch::Ptr> * orig_texture_patches,
+    std::vector<TextureAtlas::Ptr> * texture_atlases) {
+
+    std::list<TexturePatch::ConstPtr> texture_patches;
+    while (!orig_texture_patches->empty()) {
+        //TODO avoid copying
+        texture_patches.push_back(orig_texture_patches->back());
+        orig_texture_patches->pop_back();
+    }
+
+    /* Determine (tone) mapping function. */
+    float vmin, vmax;
+    std::tie(vmin, vmax) = calculate_mapping_function(texture_patches);
+
+    std::cout << "\tSorting texture patches... " << std::flush;
+    /* Improve the bin-packing algorithm efficiency by sorting texture patches
+     * in descending order of size. */
+    texture_patches.sort();
+    texture_patches.reverse();
+    std::cout << "done." << std::endl;
+
+    std::size_t const total_num_patches = texture_patches.size();
+    std::size_t remaining_patches = texture_patches.size();
+    std::ofstream tty("/dev/tty", std::ios_base::out);
+
+    #pragma omp parallel
+    {
+    #pragma omp single
+    {
+
+    while (!texture_patches.empty()) {
+        unsigned int texture_size = calculate_texture_size(texture_patches);
+
+        texture_atlases->push_back(TextureAtlas::create(texture_size));
+        TextureAtlas::Ptr texture_atlas = texture_atlases->back();
+
+        /* Try to insert each of the texture patches into the texture atlas. */
+        std::list<TexturePatch::ConstPtr>::iterator it = texture_patches.begin();
+        for (; it != texture_patches.end();) {
+            std::size_t done_patches = total_num_patches - remaining_patches;
+            int precent = static_cast<float>(done_patches)
+                / total_num_patches * 100.0f;
+            if (total_num_patches > 100
+                && done_patches % (total_num_patches / 100) == 0) {
+
+                tty << "\r\tWorking on atlas " << texture_atlases->size() << " "
+                 << precent << "%... " << std::flush;
+            }
+
+            if (texture_atlas->insert(*it, vmin, vmax)) {
+                it = texture_patches.erase(it);
+                remaining_patches -= 1;
+            } else {
+                ++it;
+            }
+        }
+
+        #pragma omp task
+        texture_atlas->finalize();
+    }
+
+    std::cout << "\r\tWorking on atlas " << texture_atlases->size()
+        << ") 100%... done." << std::endl;
+    util::WallTimer timer;
+    std::cout << "\tFinalizing texture atlases... " << std::flush;
+    #pragma omp taskwait
+    std::cout << "done. (Took: " << timer.get_elapsed_sec() << "s)" << std::endl;
+
+    /* End of single region */
+    }
+    /* End of parallel region. */
+    }
+}
+
+TEX_NAMESPACE_END

libs/tex/texture_patch.h
@@ -16,7 +16,6 @@
 #include <mve/mesh.h>
 
 #include "tri.h"
-#include "texture_view.h"
 #include "poisson_blending.h"
 
 int const texture_patch_border = 1;
@@ -28,6 +27,8 @@ int const texture_patch_border = 1;
   */
 class TexturePatch {
     public:
+        typedef std::shared_ptr<TexturePatch> Ptr;
+        typedef std::shared_ptr<const TexturePatch> ConstPtr;
         typedef std::vector<std::size_t> Faces;
         typedef std::vector<math::Vec2f> Texcoords;
 
@@ -43,7 +44,14 @@ class TexturePatch {
         /** Constructs a texture patch. */
         TexturePatch(int _label, std::vector<std::size_t> const & _faces,
             std::vector<math::Vec2f>  const & _texcoords, mve::ByteImage::Ptr _image);
+
         TexturePatch(TexturePatch const & texture_patch);
+
+        static TexturePatch::Ptr create(TexturePatch::ConstPtr texture_patch);
+        static TexturePatch::Ptr create(int label, std::vector<std::size_t> const & faces,
+            std::vector<math::Vec2f> const & texcoords, mve::ByteImage::Ptr image);
+
+        TexturePatch::Ptr duplicate(void);
 
         /** Adjust the image colors and update validity mask. */
         void adjust_colors(std::vector<math::Vec3f> const & adjust_values);
@@ -80,6 +88,22 @@ class TexturePatch {
         int get_height(void) const;
         int get_size(void) const;
 };
+
+inline TexturePatch::Ptr
+TexturePatch::create(TexturePatch::ConstPtr texture_patch) {
+    return Ptr(new TexturePatch(*texture_patch));
+}
+
+inline TexturePatch::Ptr
+TexturePatch::create(int label, std::vector<std::size_t> const & faces,
+    std::vector<math::Vec2f>  const & texcoords, mve::ByteImage::Ptr image) {
+    return Ptr(new TexturePatch(label, faces, texcoords, image));
+}
+
+inline TexturePatch::Ptr
+TexturePatch::duplicate(void) {
+    return Ptr(new TexturePatch(*this));
+}
 
 inline int
 TexturePatch::get_label(void) const {

apps/texrecon/texrecon.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 
 #include <util/timer.h>
+#include <util/system.h>
 #include <util/file_system.h>
 #include <mve/mesh_io_ply.h>
 
@@ -24,13 +25,14 @@
 #include "arguments.h"
 
 int main(int argc, char **argv) {
-
 #ifdef RESEARCH
     std::cout << "******************************************************************************" << std::endl
               << " Due to use of the -DRESEARCH=ON compile option, this program is licensed "     << std::endl
               << " for research purposes only. Please pay special attention to the gco license."  << std::endl
               << "******************************************************************************" << std::endl;
 #endif
+
+    util::system::register_segfault_handler();
 
     Timer timer;
     timer.measure("Start");
@@ -136,9 +138,10 @@ int main(int argc, char **argv) {
     }
     std::cout << "\tTook: " << wtimer.get_elapsed_sec() << "s" << std::endl;
 
-    /* Create texture patches and adjust them. */
-    tex::TexturePatches texture_patches;
+    tex::TextureAtlases texture_atlases;
     {
+        /* Create texture patches and adjust them. */
+        tex::TexturePatches texture_patches;
         tex::VertexProjectionInfos vertex_projection_infos;
         std::cout << "Generating texture patches:" << std::endl;
         tex::generate_texture_patches(graph, mesh, vertex_infos, &texture_views, &vertex_projection_infos, &texture_patches);
@@ -152,7 +155,7 @@ int main(int argc, char **argv) {
             #pragma omp parallel for schedule(dynamic)
             for (std::size_t i = 0; i < texture_patches.size(); ++i) {
                 texture_patch_counter.progress<SIMPLE>();
-                TexturePatch * texture_patch = &texture_patches[i];
+                TexturePatch::Ptr texture_patch = texture_patches[i];
                 std::vector<math::Vec3f> patch_adjust_values(texture_patch->get_faces().size() * 3, math::Vec3f(0.0f));
                 texture_patch->adjust_colors(patch_adjust_values);
                 texture_patch_counter.inc();
@@ -165,13 +168,17 @@ int main(int argc, char **argv) {
             tex::local_seam_leveling(graph, mesh, vertex_projection_infos, &texture_patches);
         }
         timer.measure("Running local seam leveling");
+
+        /* Generate texture atlases. */
+        std::cout << "Generating texture atlases:" << std::endl;
+        tex::generate_texture_atlases(&texture_patches, &texture_atlases);
     }
 
     /* Create and write out obj model. */
     {
         std::cout << "Building objmodel:" << std::endl;
         tex::Model model;
-        tex::build_model(mesh, texture_patches, &model);
+        tex::build_model(mesh, texture_atlases, &model);
         timer.measure("Building OBJ model");
 
         std::cout << "\tSaving model... " << std::flush;
@@ -187,18 +194,20 @@ int main(int argc, char **argv) {
     }
 
     if (conf.write_view_selection_model) {
+        texture_atlases.clear();
         std::cout << "Generating debug texture patches:" << std::endl;
         {
-            texture_patches.clear();
+            tex::TexturePatches texture_patches;
             generate_debug_embeddings(&texture_views);
             tex::VertexProjectionInfos vertex_projection_infos; // Will only be written
             tex::generate_texture_patches(graph, mesh, vertex_infos, &texture_views, &vertex_projection_infos, &texture_patches);
+            tex::generate_texture_atlases(&texture_patches, &texture_atlases);
         }
 
         std::cout << "Building debug objmodel:" << std::endl;
         {
             tex::Model model;
-            tex::build_model(mesh, texture_patches, &model);
+            tex::build_model(mesh, texture_atlases, &model);
             std::cout << "\tSaving model... " << std::flush;
             tex::Model::save(model, conf.out_prefix + "_view_selection");
             std::cout << "done." << std::endl;

commit 78561f9a25b83d9d9343b3390b013cadf37afb5b

Fix sorting of texture patches, broke in 40a0e42(use of Ptr)

libs/tex/generate_texture_atlases.cpp
@@ -51,7 +51,10 @@ calculate_texture_size(std::list<TexturePatch::ConstPtr> const & texture_patches
             unsigned int area = width * height;
             unsigned int waste = area - texture_patch->get_size();
 
+            /* Only consider patches where the information dominates padding. */
             if (static_cast<double>(waste) / texture_patch->get_size() > 1.0) {
+                /* Since the patches are sorted by size we can assume that only
+                 * few further patches will contribute to the size and break. */
                 break;
             }
 
@@ -112,6 +115,10 @@ calculate_mapping_function(std::list<TexturePatch::ConstPtr> const & texture_pat
     return std::pair<float, float>(min, max);
 }
 
+bool comp(TexturePatch::ConstPtr first, TexturePatch::ConstPtr second) {
+    return first->get_size() > second->get_size();
+}
+
 void
 generate_texture_atlases(std::vector<TexturePatch::Ptr> * orig_texture_patches,
     std::vector<TextureAtlas::Ptr> * texture_atlases) {
@@ -130,8 +137,7 @@ generate_texture_atlases(std::vector<TexturePatch::Ptr> * orig_texture_patches,
     std::cout << "\tSorting texture patches... " << std::flush;
     /* Improve the bin-packing algorithm efficiency by sorting texture patches
      * in descending order of size. */
-    texture_patches.sort();
-    texture_patches.reverse();
+    texture_patches.sort(comp);
     std::cout << "done." << std::endl;
 
     std::size_t const total_num_patches = texture_patches.size();

libs/tex/texture_patch.h
@@ -56,9 +56,6 @@ class TexturePatch {
         /** Adjust the image colors and update validity mask. */
         void adjust_colors(std::vector<math::Vec3f> const & adjust_values);
 
-        /** Compares texture patches image size. */
-        bool operator<(TexturePatch const & texture_patch) const;
-
         math::Vec3f get_pixel_value(math::Vec2f pixel) const;
         void set_pixel_value(math::Vec2i pixel, math::Vec3f color);
 
@@ -165,11 +162,6 @@ TexturePatch::get_faces(void) const {
 inline int
 TexturePatch::get_size(void) const {
     return get_width() * get_height();
-}
-
-inline bool
-TexturePatch::operator<(TexturePatch const & other) const {
-    return get_size() < other.get_size();
 }
 
 #endif /* TEX_TEXTUREPATCH_HEADER */