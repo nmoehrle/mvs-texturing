nmoehrle/mvs-texturing

commit a03ea60374829437b1f41ab4587daf62f36a59a2

Properly estimate holes patch size

libs/tex/generate_texture_patches.cpp
@@ -7,12 +7,12 @@ void merge_vertex_projection_infos(std::vector<std::vector<VertexProjectionInfo>
     /* Merge vertex infos within the same texture patch. */
     #pragma omp parallel for
     for (std::size_t i = 0; i < vertex_projection_infos->size(); ++i) {
-        std::vector<VertexProjectionInfo> * infos = &(vertex_projection_infos->at(i));
+        std::vector<VertexProjectionInfo> & infos = vertex_projection_infos->at(i);
 
         std::map<std::size_t, VertexProjectionInfo> info_map;
         std::map<std::size_t, VertexProjectionInfo>::iterator it;
 
-        for (VertexProjectionInfo const & info : *infos) {
+        for (VertexProjectionInfo const & info : infos) {
             std::size_t texture_patch_id = info.texture_patch_id;
             if((it = info_map.find(texture_patch_id)) == info_map.end()) {
                 info_map[texture_patch_id] = info;
@@ -22,10 +22,10 @@ void merge_vertex_projection_infos(std::vector<std::vector<VertexProjectionInfo>
             }
         }
 
-        infos->clear();
-        infos->reserve(info_map.size());
+        infos.clear();
+        infos.reserve(info_map.size());
         for (it = info_map.begin(); it != info_map.end(); ++it) {
-            infos->push_back(it->second);
+            infos.push_back(it->second);
         }
     }
 }
@@ -170,6 +170,7 @@ generate_texture_patches(UniGraph const & graph, std::vector<TextureView> const 
         }
     }
 
+    merge_vertex_projection_infos(vertex_projection_infos);
 
     std::size_t num_holes = 0;
     std::size_t num_hole_faces = 0;
@@ -326,15 +327,37 @@ generate_texture_patches(UniGraph const & graph, std::vector<TextureView> const 
             }
 
             float total_length = 0.0f;
+            float total_projection_length = 0.0f;
             for (std::size_t j = 0; j < border.size(); ++j) {
-                math::Vec3f const & v0 = vertices[border[j]];
-                math::Vec3f const & v1 = vertices[border[(j + 1) % border.size()]];
+                std::size_t vi0 = border[j];
+                std::size_t vi1 = border[(j + 1) % border.size()];
+                std::vector<VertexProjectionInfo> const & vpi0 = vertex_projection_infos->at(vi0);
+                std::vector<VertexProjectionInfo> const & vpi1 = vertex_projection_infos->at(vi0);
+                /* According to the previous checks (vertex class within the origial
+                 * mesh and boundary) there already has to be at least one projection
+                 * of each border vertex. */
+                assert(!vpi0.empty() && !vpi1.empty());
+                math::Vec2f vp0(0.0f), vp1(0.0f);
+                for (VertexProjectionInfo const & info0 : vpi0) {
+                    for (VertexProjectionInfo const & info1 : vpi1) {
+                        if (info0.texture_patch_id == info1.texture_patch_id) {
+                            vp0 = info0.projection;
+                            vp1 = info1.projection;
+                            break;
+                        }
+                    }
+                }
+                total_projection_length += (vp0 - vp1).norm();
+                math::Vec3f const & v0 = vertices[vi0];
+                math::Vec3f const & v1 = vertices[vi1];
                 total_length += (v0 - v1).norm();
             }
+            float radius = total_projection_length / (2.0f * MATH_PI);
+
             float length = 0.0f;
             std::vector<math::Vec2f> projections(num_vertices);
             for (std::size_t j = 0; j < border.size(); ++j) {
-                float angle = 2.0f * MATH_PI * length / total_length;
+                float angle = 2.0f * MATH_PI * (length / total_length);
                 projections[g2l[border[j]]] = math::Vec2f(std::cos(angle), std::sin(angle));
                 math::Vec3f const & v0 = vertices[border[j]];
                 math::Vec3f const & v1 = vertices[border[(j + 1) % border.size()]];
@@ -408,7 +431,7 @@ generate_texture_patches(UniGraph const & graph, std::vector<TextureView> const 
                 xy = solver.solve(by);
             }
 
-            int image_size = 100; //TODO better estimate
+            int image_size = std::floor(radius * 1.1f) * 2 + 4;
             int scale = image_size / 2 - texture_patch_border;
             for (std::size_t j = 0, k = 0; j < num_vertices; ++j) {
                 if (!is_border[j]) {
@@ -420,6 +443,7 @@ generate_texture_patches(UniGraph const & graph, std::vector<TextureView> const 
             }
 
             mve::ByteImage::Ptr image = mve::ByteImage::create(image_size, image_size, 3);
+            //DEBUG image->fill_color(*math::Vec4uc(0, 255, 0, 255));
             std::vector<math::Vec2f> texcoords; texcoords.reserve(subgraph.size());
             for (std::size_t const face_id : subgraph) {
                 for (std::size_t j = 0; j < 3; ++j) {

libs/tex/local_seam_leveling.cpp
@@ -131,7 +131,6 @@ local_seam_leveling(UniGraph const & graph, mve::TriangleMesh::ConstPtr mesh,
 
         texture_patch->blend(orig_texture_patches[i].get_image());
         texture_patch->release_blending_mask();
-        texture_patch->erode_validity_mask();
         texture_patch_counter.inc();
     }
 }

libs/mrf/GCOGraph.cpp
@@ -29,7 +29,12 @@ void GCOGraph::set_data_costs(int label, std::vector<SparseDataCost> const & cos
         gcocosts[i] = {costs[i].site, costs[i].cost};
     }
     std::sort(gcocosts.begin(), gcocosts.end(), comp_spd_site);
-    gco.setDataCost(label, gcocosts.data(), gcocosts.size());
+    try {
+        gco.setDataCost(label, gcocosts.data(), gcocosts.size());
+    } catch (GCException e) {
+        std::cerr << e.message << std::endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 ENERGY_TYPE GCOGraph::compute_energy() {

libs/tex/TexturePatch.cpp
@@ -57,10 +57,11 @@ TexturePatch::adjust_colors(std::vector<math::Vec3f> const & adjust_values) {
                 bool inside = bcoords.minimum() >= 0.0f;
                 if (inside) {
                     assert(x != 0 && y != 0);
-                    for (int c = 0; c < 3; ++c)
+                    for (int c = 0; c < 3; ++c) {
                         iadjust_values->at(x, y, c) = math::interpolate(
                             adjust_values[i][c], adjust_values[i + 1][c], adjust_values[i + 2][c],
                             bcoords[0], bcoords[1], bcoords[2]);
+                    }
                     validity_mask->at(x, y, 0) = 255;
                     blending_mask->at(x, y, 0) = 255;
                 } else {
@@ -76,10 +77,11 @@ TexturePatch::adjust_colors(std::vector<math::Vec3f> const & adjust_values) {
                     if (ha > sqrt_2 || hb > sqrt_2 || hc > sqrt_2)
                         continue;
 
-                    for (int c = 0; c < 3; ++c)
+                    for (int c = 0; c < 3; ++c) {
                         iadjust_values->at(x, y, c) = math::interpolate(
                             adjust_values[i][c], adjust_values[i + 1][c], adjust_values[i + 2][c],
                             bcoords[0], bcoords[1], bcoords[2]);
+                    }
                     validity_mask->at(x, y, 0) = 255;
                     blending_mask->at(x, y, 0) = 128;
                 }
@@ -89,12 +91,13 @@ TexturePatch::adjust_colors(std::vector<math::Vec3f> const & adjust_values) {
 
     for (int i = 0; i < image->get_pixel_amount(); ++i) {
         if (validity_mask->at(i, 0) != 0){
-            for (int c = 0; c < 3; ++c)
+            for (int c = 0; c < 3; ++c) {
                 image->at(i, c) += iadjust_values->at(i, c);
+            }
         } else {
-            image->at(i, 0) = 1.0f;
-            image->at(i, 1) = 0.0f;
-            image->at(i, 2) = 1.0f;
+            math::Vec3f color(0.0f, 0.0f, 0.0f);
+            //DEBUG math::Vec3f color(1.0f, 0.0f, 1.0f);
+            std::copy(color.begin(), color.end(), &image->at(i, 0));
         }
     }
 }
@@ -164,6 +167,15 @@ TexturePatch::set_pixel_value(math::Vec2i pixel, math::Vec3f color) {
 void
 TexturePatch::blend(mve::FloatImage::ConstPtr orig) {
     poisson_blend(orig, blending_mask, image, 1.0f);
+
+    /* Invalidate all pixels outside of the boundary. */
+    for (int y = 0; y < blending_mask->height(); ++y) {
+        for (int x = 0; x < blending_mask->width(); ++x) {
+            if (blending_mask->at(x, y, 0) == 128) {
+                validity_mask->at(x, y, 0) = 0;
+            }
+        }
+    }
 }
 
 void
@@ -250,38 +262,6 @@ TexturePatch::prepare_blending_mask(std::size_t strip_width){
          int x = it->first;
          int y = it->second;
 
-         blending_mask->at(x, y, 0) = 128;
+         blending_mask->at(x, y, 0) = 126;
     }
-}
-
-void
-TexturePatch::erode_validity_mask(void) {
-
-    int const width = validity_mask->width();
-    int const height = validity_mask->height();
-
-    mve::ByteImage::Ptr eroded_validity_mask = validity_mask->duplicate();
-
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-                eroded_validity_mask->at(x, y, 0) = 0;
-
-            bool invalid = validity_mask->at(x, y, 0) == 0;
-            for (int j = -1; j <= 1 && !invalid; ++j) {
-                for (int i = -1; i <= 1 && !invalid; ++i) {
-                    int nx = x + i;
-                    int ny = y + j;
-                    if (0 <= nx && nx < width &&
-                        0 <= ny && ny < height &&
-                        validity_mask->at(nx, ny, 0) == 0) {
-                        eroded_validity_mask->at(x, y, 0) = 0;
-                        invalid = true;
-                    }
-                }
-            }
-        }
-    }
-
-    validity_mask.swap(eroded_validity_mask);
 }

libs/tex/build_obj_model.cpp
@@ -35,7 +35,7 @@ calculate_texture_size(std::list<TexturePatch> & texture_patches) {
 
     /* Heuristic to determine a proper texture size. */
     /* Approximate area which will be needed */
-    double approx_area = 1.4 * area;
+    double approx_area = 1.5 * area;
 
     /* Probably fits into a single texture atlas. */
     if (approx_area < MAX_TEXTURE_SIZE * MAX_TEXTURE_SIZE) {
@@ -71,8 +71,9 @@ void copy_into(mve::ByteImage::ConstPtr src, int x, int y,
             if (sx < 0 || sx >= src->width() || sy < 0 || sy >= src->height())
                 continue;
 
-            for (int c = 0; c < src->channels(); ++c)
+            for (int c = 0; c < src->channels(); ++c) {
                 dest->at(x + i, y + j, c) = src->at(sx, sy, c);
+            }
         }
     }
 }
@@ -131,7 +132,7 @@ dilate_valid_pixel(mve::ByteImage::Ptr image, mve::ByteImage::Ptr validity_mask)
     mve::ByteImage::Ptr new_validity_mask = validity_mask->duplicate();
 
     /* Iteratively dilate border pixels until padding constants are reached. */
-    for (int n = 0; n < (size >> 7); ++n) {
+    for (int n = 0; n <= (size >> 7); ++n) {
         PixelVector new_valid_pixels;
 
         PixelSet::iterator it = invalid_border_pixels.begin();

libs/tex/seam_leveling.h
@@ -13,6 +13,10 @@ struct VertexProjectionInfo {
     std::size_t texture_patch_id;
     math::Vec2f projection;
     std::vector<std::size_t> faces;
+
+    bool operator<(VertexProjectionInfo const & other) const {
+        return texture_patch_id < other.texture_patch_id;
+    }
 };
 
 struct ProjectedEdgeInfo {
@@ -20,8 +24,8 @@ struct ProjectedEdgeInfo {
     math::Vec2f p1;
     math::Vec2f p2;
 
-    bool operator<(ProjectedEdgeInfo const & projected_edge_info) const {
-        return texture_patch_id < projected_edge_info.texture_patch_id;
+    bool operator<(ProjectedEdgeInfo const & other) const {
+        return texture_patch_id < other.texture_patch_id;
     }
 };
 

commit 544f3717a451948e9e3d1ff270bb4609bb24def5

Fix hole filling, broke in a03ea60, 89f1bf1

libs/tex/generate_texture_patches.cpp
@@ -255,7 +255,7 @@ bool fill_hole(std::vector<std::size_t> const & hole, UniGraph const & graph,
         std::size_t vi0 = border[j];
         std::size_t vi1 = border[(j + 1) % border.size()];
         std::vector<VertexProjectionInfo> const & vpi0 = vertex_projection_infos->at(vi0);
-        std::vector<VertexProjectionInfo> const & vpi1 = vertex_projection_infos->at(vi0);
+        std::vector<VertexProjectionInfo> const & vpi1 = vertex_projection_infos->at(vi1);
         /* According to the previous checks (vertex class within the origial
          * mesh and boundary) there already has to be at least one projection
          * of each border vertex. */

libs/tex/local_seam_leveling.cpp
@@ -154,6 +154,7 @@ local_seam_leveling(UniGraph const & graph, mve::TriangleMesh::ConstPtr mesh,
     /* Sample vertex colors. */
     for (std::size_t i = 0; i < vertex_colors.size(); ++i) {
         std::vector<VertexProjectionInfo> const & projection_infos = vertex_projection_infos[i];
+        if (projection_infos.size() <= 1) continue;
 
         math::Accum<math::Vec3f> color_accum(math::Vec3f(0.0f));
         for (VertexProjectionInfo const & projection_info : projection_infos) {
@@ -162,6 +163,8 @@ local_seam_leveling(UniGraph const & graph, mve::TriangleMesh::ConstPtr mesh,
             math::Vec3f color = texture_patch->get_pixel_value(projection_info.projection);
             color_accum.add(color, 1.0f);
         }
+	if (color_accum.w == 0.0f) continue;
+
         vertex_colors[i] = color_accum.normalized();
 
         for (VertexProjectionInfo const & projection_info : projection_infos) {
@@ -193,6 +196,7 @@ local_seam_leveling(UniGraph const & graph, mve::TriangleMesh::ConstPtr mesh,
         if (texture_patch->get_label() != 0) {
             texture_patch->prepare_blending_mask(STRIP_SIZE);
         }
+
         texture_patch->blend(image);
         texture_patch->release_blending_mask();
         texture_patch_counter.inc();