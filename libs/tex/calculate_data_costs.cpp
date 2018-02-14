3cc4357899ba0c7b7f1d7b1d9c8eb23c0cdad4ff
Piero Toffanin <pt@masseranolabs.com>
2018-02-14 10:41:40 -0500

    Increased cutoff angle when visibility test is disabled (#1)

--- libs/tex/calculate_data_costs.cpp
+++ libs/tex/calculate_data_costs.cpp
@@ -184,7 +184,8 @@ calculate_face_projection_infos(mve::TriangleMesh::ConstPtr mesh,
                 if (viewing_angle < 0.0f || viewing_direction.dot(view_to_face_vec) < 0.0f)
                     continue;
 
-                if (std::acos(viewing_angle) > MATH_DEG2RAD(75.0f))
+                float cutoffAngle = settings.geometric_visibility_test ? 75.0f : 90.0f;
+                if (std::acos(viewing_angle) > MATH_DEG2RAD(cutoffAngle))
                     continue;
 
                 /* Projects into the valid part of the TextureView? */
